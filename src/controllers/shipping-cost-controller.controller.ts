b93b3e40bf6a10ea510103cf3f1e12a4815d78ee
Silvestre Moreno <silmoreno70@gmail.com>
2021-11-03 19:33:09 -0600

    -Return shipping cost with discount applied

--- src/controllers/shipping-cost-controller.controller.ts
+++ src/controllers/shipping-cost-controller.controller.ts
@@ -36,114 +36,27 @@ export class ShippingCostController {
     })
     data: PaymentData,
   ): Promise<object> {
-    const stateFound = await this.postalCodeRepository.findOne({
+    /* const stateFound = await this.postalCodeRepository.findOne({
       where: {code: data.postalCode},
-    });
-    const zoneFound = await this.zoneRepository.findOne({
-      where: {states: stateFound?.state},
-    });
-    const couponFound = await this.couponRepository.findOne({
-      where: {code: data.coupon},
-    });
+    }); */
+    const zoneFound = await this.zoneRepository.findById(data.idZone);
     let shippingCost = 0;
     const kilos = Math.trunc(data.weight);
-    if (data.cost > 10000) {
-      shippingCost = 0;
+    if (kilos <= 8) {
+      shippingCost = (zoneFound?.priceKilos as Record<string, number>)[
+        Math.trunc(kilos).toString()
+      ];
     } else {
-      if (kilos <= 8) {
-        shippingCost = (zoneFound?.priceKilos as Record<string, number>)[
-          Math.trunc(kilos).toString()
-        ];
-      } else {
-        const extraKilos = kilos - 8;
-        shippingCost =
-          (zoneFound?.priceKilos as Record<string, number>)['8'] +
-          (extraKilos *
-            (zoneFound?.priceKilos as Record<string, number>)['extra']);
-      }
+      const extraKilos = kilos - 8;
+      shippingCost =
+        (zoneFound?.priceKilos as Record<string, number>)['8'] +
+        (extraKilos *
+          (zoneFound?.priceKilos as Record<string, number>)['extra']);
     }
-    if (
-      zoneFound?.name === 'Zona 4' &&
-      data.cost > 3000 &&
-      data.paymentMethod === 'visa'
-    ) {
-      return {
-        shippingCost: 0,
-        cost: data.cost,
-        total: data.cost,
-        estimatedTime: zoneFound?.shippingTime
-      };
-    }
-    if (
-      zoneFound?.name === 'Zona 3' &&
-      data.cost > 4000 &&
-      data.paymentMethod === 'visa'
-    ) {
-      const cost = data.cost * 0.85;
-      return {
-        shippingCost,
-        cost: cost,
-        total: cost,
-        estimatedTime: zoneFound?.shippingTime
-      };
-    }
-    if (
-      (zoneFound?.name === 'Zona 1' || zoneFound?.name === 'Zone 2') &&
-      data.paymentMethod === 'mastercard' &&
-      couponFound &&
-      couponFound?.name === 'MASTER20'
-    ) {
-      const cost = data.cost * 0.9;
-      return {
-        shippingCost,
-        cost: cost,
-        total: cost + shippingCost,
-        estimatedTime: zoneFound?.shippingTime
-      };
-    }
-    if (
-      (zoneFound?.name === 'Zona 1' ||
-        zoneFound?.name === 'Zona 2' ||
-        zoneFound?.name === 'Zona 3') &&
-      (data.paymentMethod === 'visa' || data.paymentMethod === 'mastercard') &&
-      couponFound &&
-      couponFound?.name === 'PERRITOFELI'
-    ) {
-      const shippingDiscount = shippingCost * 0.85;
-      return {
-        shippingCost: shippingDiscount,
-        cost: data.cost,
-        total: data.cost + shippingDiscount,
-        estimatedTime: zoneFound?.shippingTime
-      };
-    }
-    if (zoneFound?.name === 'Zona 5' && data.paymentMethod === 'mastercard') {
-      return {
-        shippingCost,
-        cost: data.cost,
-        total: data.cost + shippingCost,
-        estimatedTime: zoneFound?.shippingTime
-      };
-    }
-    if (
-      (zoneFound?.name === 'Zona 4' || zoneFound?.name === 'Zona 5') &&
-      couponFound &&
-      couponFound?.name === 'NOJADO'
-    ) {
-      const cost = data.cost * 0.85;
-      return {
-        shippingCost,
-        cost: cost,
-        total: cost + shippingCost,
-        estimatedTime: zoneFound?.shippingTime
-      };
-    }
+    shippingCost = (shippingCost * data.discount) / 100
     return {
-      shippingCost,
-      cost: data.cost,
-      total: data.cost + shippingCost,
-      estimatedTime: zoneFound?.shippingTime
-    };
+      shippingCost
+    }
   }
   @get('/getZone/{postalCode}')
   @response(200, {
